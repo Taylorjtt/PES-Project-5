4944ca5641e75cce4fe732b273dad310dc286768
John Taylor <taylorjtt@gmail.com>
2019-11-19 07:58:09 -0700

    Adding function header comments

--- Project_5/source/RingBuffer/RingBuffer.h
+++ Project_5/source/RingBuffer/RingBuffer.h
@@ -29,14 +29,80 @@ typedef struct RING_BUFF_OBJ
 
 typedef struct RING_BUFF_OBJ *RingBufferHandle;
 
+/**
+* @brief Constructor for RingBuffer object
+*
+* Given a chunk of memory, the size of an RingBuffer object, initialize a RingBuffer
+*
+* @param pmemory	The pointer to the memory that this object will use
+* @param numbytes	The expected size of the object
+* @param bufferSize	bufferSize initial size of the buffer
+* @return RingBufferHandle a pointer to a RingBuffer
+*/
 RingBufferHandle RingBuffer_Constructor(void *pmemory, const size_t numbytes, const size_t bufferSize);
+/**
+* @brief push a byte to the ringbuffer
+*
+*
+* @param handle	pointer to a RingBuffer
+* @param data	data to push
+* @return BUFFER_ERROR success/fail
+*/
 BUFFER_ERROR RingBuffer_push(RingBufferHandle handle, uint8_t data);
+/**
+* @brief pop a byte off the ringbuffer
+*
+*
+* @param handle	pointer to a RingBuffer
+* @return BUFFER_ERROR success/fail
+*/
 BUFFER_ERROR RingBuffer_pop(RingBufferHandle handle);
+/**
+* @brief read the next value in the queue
+*
+*
+* @param handle	pointer to a RingBuffer
+* @return the next char
+*/
 uint8_t RingBuffer_peek(RingBufferHandle handle);
+/**
+* @brief get the size of the ringBuffer
+*
+*
+* @param handle	pointer to a RingBuffer
+* @return size of the buffer
+*/
 size_t RingBuffer_getSize(RingBufferHandle handle);
+/**
+* @brief return if the buffer is Full
+*
+*
+* @param handle	pointer to a RingBuffer
+* @return true if full, false if not full
+*/
 bool RingBuffer_isFull(RingBufferHandle handle);
+/**
+* @brief return if the buffer is empty
+*
+*
+* @param handle	pointer to a RingBuffer
+* @return false if not empty, true if empty
+*/
 bool RingBuffer_isEmpty(RingBufferHandle handle);
+/**
+* @brief return if the buffer is valid
+*
+*
+* @param handle	pointer to a RingBuffer
+* @return false if not valid, true if valid
+*/
 bool RingBuffer_isValid(RingBufferHandle handle);
+/**
+* @brief destroy the buffer
+*
+*
+* @param handle	pointer to a RingBuffer
+*/
 void RingBuffer_destroy(RingBufferHandle handle);
 
 
--- Project_5/source/UART/UART.h
+++ Project_5/source/UART/UART.h
@@ -43,16 +43,103 @@ typedef struct _UART_OBJ_
 }UART_OBJ;
 
 typedef struct _UART_OBJ_ *UARTHandle;
+/**
+* @brief Constructor for UART object
+*
+* Given a chunk of memory, the size of an UART object, initialize a UART
+*
+* @param pmemory		The pointer to the memory that this object will use
+* @param numbytes		The expected size of the object
+* @param systemClock	System clock speed for baud calculation
+* @param baud			desired baud
+* @param led			led handle
+* @return UARTHandle 	a pointer to a UART
+*/
 UARTHandle UART_constructor(void *pmemory, const size_t numBytes,uint32_t systemClock, uint32_t baud, RGBLEDHandle led);
+/**
+* @brief get the latest char in the readBuffer
+*
+*
+* @param handle	pointer to a UARTHandle
+* @return the latest data
+*/
 uint8_t UART_getChar(UARTHandle handle);
+/**
+* @brief put a char in the tx register
+*
+*
+* @param handle	pointer to a UARTHandle
+* @param data to send
+*/
 void UART_putChar(UARTHandle handle, uint8_t c);
+/**
+* @brief is the transmitter ready to be used
+*
+*
+* @param handle	pointer to a UARTHandle
+* @return whether or not tx is ready to send
+*/
 bool UART_txAvailable(UARTHandle handle);
-void UART_printf(UARTHandle handle, const char*, ...);
-char *convert(unsigned int num, int base);
+/**
+* @brief is the rx ready to be used
+*
+*
+* @param handle	pointer to a UARTHandle
+* @return whether or not rx is ready to recieve
+*/
 bool UART_rxAvailable(UARTHandle handle);
-void UART_queueChar(UARTHandle handle,char c);
-void UART_enableTXInterrupt(UARTHandle handle);
-void UART_disableTXInterrupt(UARTHandle handle);
-void UART_queueString(UARTHandle handle,char* string);
+/**
+* @brief is the latest tx done
+*
+*
+* @param handle	pointer to a UARTHandle
+* @return whether or not tx is done sending
+*/
 bool UART_transmitComplete(UARTHandle handle);
+/**
+* @brief add a char to the tx ringBuffer
+*
+*
+* @param handle	pointer to a UARTHandle
+* @param char to add
+*/
+void UART_queueChar(UARTHandle handle,char c);
+/**
+* @brief enable tx interrupt
+*
+*
+* @param handle	pointer to a UARTHandle
+*/
+void UART_enableTXInterrupt(UARTHandle handle);
+/**
+* @brief disable tx interrupt
+*
+*
+* @param handle	pointer to a UARTHandle
+*/
+void UART_disableTXInterrupt(UARTHandle handle);
+/**
+* @brief add a string to the tx ringbuffer
+*
+*
+* @param handle	pointer to a UARTHandle
+* @param string to add
+*/
+void UART_queueString(UARTHandle handle,char* string);
+/**
+* @brief a printf-like function for this project
+*
+*
+* @param handle	pointer to a UARTHandle
+* @param variable amount of arguments (like printf)
+*/
+void UART_printf(UARTHandle handle, const char*, ...);
+/**
+* @brief convert a number to a particular base
+*
+*
+* @param num	number to convert
+* @param base	base to convert to
+*/
+char *convert(unsigned int num, int base);
 #endif /* UART_UART_H_ */
